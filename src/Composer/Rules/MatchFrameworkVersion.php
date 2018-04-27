51fdadc9d3bae6ce2eebbff61d8a500aa9597e2d
Maxime Rainville <maxime@silverstripe.com>
2018-04-27 17:56:17 +1200

    Get started ona rule to upgrade constraint on silverstripe modules.

---
+++ src/Composer/Rules/MatchFrameworkVersion.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace SilverStripe\Upgrader\Composer\Rules;
+
+use SilverStripe\Upgrader\Composer\Package;
+
+/**
+ * Rule to go through the require list and update the constraint to work with a specific version of Framework.
+ */
+class MatchFrameworkVersion implements DependencyUpgradeRule {
+
+    protected $targetFramework;
+
+    /**
+     * Instanciate a new MatchFramworkVersion Upgrade Rule.
+     * @param string $constraint
+     */
+    public function __construct($targetFramework)
+    {
+        $this->targetFramework = $targetFramework;
+    }
+
+
+    /**
+     * @inheritDoc
+     * @param  array $dependencies Dependencies to upgrade
+     * @return array Upgraded dependencies
+     */
+    public function upgrade(array $dependencies): array
+    {
+        foreach ($dependencies as $packageName => $constrain) {
+
+            $package = new Package($packageName);
+
+            switch ($package->getType()) {
+                case 'silverstripe-module':
+                case 'silverstripe-vendormodule':
+                    $dependencies[$packageName] =
+                        $this->upgradeModule($package) ?:
+                        $dependencies[$packageName];
+                    break;
+            }
+        }
+
+        return $dependencies;
+    }
+
+    /**
+     * Find the latest version that is compatible with our targeted version of Framework.
+     * @param  Package $package
+     * @return string
+     */
+    protected function upgradeModule(Package $package): string
+    {
+        $nextVersion = $package->getVersionBySilverStripeCompatibility($this->targetFramework);
+        if ($nextVersion) {
+            $versionString = $nextVersion->getId();
+            if (preg_match('/^(\d+\.)*\d+$/', $versionString)) {
+                $versionString = '^' . $versionString;
+            }
+
+            return $versionString;
+        }
+
+        return '';
+    }
+
+}
---
+++ tests/Composer/Rules/MatchFrameworkVersionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SilverStripe\Upgrader\Tests\Composer\Rules;
+
+use PHPUnit\Framework\TestCase;
+use SilverStripe\Upgrader\Composer\Rules\MatchFrameworkVersion;
+use SilverStripe\Upgrader\Tests\Composer\InitPackageCacheTrait;
+
+class MatchFrameworkVersionTest extends TestCase
+{
+
+    use InitPackageCacheTrait;
+
+    public function testUpgrade()
+    {
+        $rule = new MatchFrameworkVersion('3.6.0');
+
+
+        $this->assertEquals(
+            $rule->upgrade(['silverstripe/cms' => '^3.2']),
+            ['silverstripe/cms' => '^3.6']
+        );
+
+    }
+
+}
