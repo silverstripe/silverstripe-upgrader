cffd2f0d42557a21e43f92bd4ef222641ba76d22
Maxime Rainville <maxime@silverstripe.com>
2018-04-30 17:02:15 +1200

    Clean up and write test for ComposerFile.

--- src/Composer/ComposerFile.php
+++ src/Composer/ComposerFile.php
@@ -6,9 +6,12 @@ use SilverStripe\Upgrader\CodeCollection\DiskItem;
 use InvalidArgumentException;
 
 /**
- * Represent a `composer.json` file and provide methods to interact with it.
+ * Represent a `composer.json` file and provide methods to interact with it. Note that this object doesn't
+ * autoamtically detect changes to the composer file. So you may need to call the `parse` method after performing an
+ * operation on this file to get the latest values.
  */
-class ComposerFile extends DiskItem {
+class ComposerFile extends DiskItem
+{
 
     use TemporaryFile;
 
@@ -33,24 +36,68 @@ class ComposerFile extends DiskItem {
     protected $exec;
 
     /**
-     *
-     * @param [type] $basePath     [description]
-     * @param string $relativePath [description]
+     * Flag to indicate this is a temporary composer file that doesn't need to be retained after execution.
+     * @var bool
      */
-    public function __construct(ComposerExec $exec, $basePath)
+    protected $temporary;
+
+    /**
+     * Instanciate a new ComposerFile
+     * @param ComposerExec $exec Composer executable to use when running operation on this file.
+     * @param string $basePath Directory containing this composer file.
+     * @param bool $temporaryProject Whatever this schema should be retain after execution.
+     */
+    public function __construct(ComposerExec $exec, string $basePath, bool $temporary = false)
     {
         parent::__construct($basePath, 'composer.json');
         $this->exec = $exec;
+        $this->temporary = $temporary;
         $this->parse();
     }
 
     /**
+     * Self-destruct the project if it's meant to be temporary.
+     */
+    public function __destruct()
+    {
+        if ($this->temporary) {
+            $this->delDir($this->getBasePath());
+        }
+    }
+
+    /**
+     * Delete a directory and all of its content.
+     * @internal PHP's `rmdir` thrown an error if you try to target constains other file. Copied from
+     * https://stackoverflow.com/a/3349792/1427439
+     * @param string $dirPath
+     */
+    private function delDir(string $dirPath)
+    {
+        if (! is_dir($dirPath)) {
+            throw new InvalidArgumentException("$dirPath must be a directory");
+        }
+        if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+            $dirPath .= '/';
+        }
+        $files = glob($dirPath . '*', GLOB_MARK);
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                $this->delDir($file);
+            } else {
+                unlink($file);
+            }
+        }
+        rmdir($dirPath);
+    }
+
+    /**
      * Try to parse the composer file content.
+     * @throws InvalidArgumentException
      */
     public function parse()
     {
         if ($this->exec->validate($this->getFullPath())) {
-            $this->composerJson = json_decode($this->getContents(), TRUE);
+            $this->composerJson = json_decode($this->getContents(), true);
         } else {
             throw new InvalidArgumentException('Invalid composer file at ' . $this->getFullPath());
         }
@@ -61,7 +108,7 @@ class ComposerFile extends DiskItem {
      * @param  array|string|null $content
      * @return boolean
      */
-    public function validate($content=null)
+    public function validate($content = null)
     {
         // If we haven't been provided any content, assume we are validating the current file.
         if ($content === null) {
@@ -70,6 +117,12 @@ class ComposerFile extends DiskItem {
 
         // If we have an array, JSON code it
         if (is_array($content)) {
+            // This is a bit of silly hack. If require is empty it will be outputted. as an array. `composer validate`
+            // expect it to be an object, so we just had a dummy key pair in there to make sure it always generated as
+            // an object.
+            if (isset($content['require']) && empty($content['require'])) {
+                $content['require']['php'] = '*';
+            }
             $content = json_encode($content);
         }
 
@@ -84,9 +137,12 @@ class ComposerFile extends DiskItem {
         return $this->exec->validate($path);
     }
 
-    public function getRequire()
+    /**
+     * Get the requirements as defined by the `require` key in the composer file.
+     * @return array
+     */
+    public function getRequire(): array
     {
         return $this->composerJson['require'];
     }
-
 }
--- tests/Composer/ComposerFileTest.php
+++ tests/Composer/ComposerFileTest.php
@@ -44,4 +44,39 @@ class ComposerFileTest extends TestCase
         );
     }
 
+    public function testParseAndRequire()
+    {
+        $composer = new ComposerExec(__DIR__);
+        $schema = $composer->initTemporarySchema();
+        $composer->setWorkingDir($schema->getBasePath());
+
+        $this->assertEmpty($schema->getRequire(), 'getRequire should be empty when run against an empty file.');
+        $composer->require('php', '*');
+
+        // Test parse and require after updating the composer file.
+        $schema->parse();
+        $this->assertEquals(
+            $schema->getRequire(),
+            ['php' => '*'],
+            'Parsing an updated file should cause getRequire to get the latest value.'
+        );
+    }
+
+    public function testInvalidParse()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        // Let's create a brand new composer file and then break it on purpose.
+        $composer = new ComposerExec(__DIR__);
+        $schema = $composer->initTemporarySchema();
+
+        $schema->setContents(
+            $schema->getContents() .
+            ' some random text that will break our JSON file.'
+        );
+
+
+        // Parsing hte file now should throw an exception.
+        $schema->parse();
+    }
 }
