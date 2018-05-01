f69b60a314205356ad0f41b8eeba0ded7fbad66d
Maxime Rainville <maxime@silverstripe.com>
2018-05-01 17:26:22 +1200

    Add a new strict rule.

---
+++ src/Composer/Rules/StrictVersion.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SilverStripe\Upgrader\Composer\Rules;
+
+use Composer\Semver\Semver;
+use SilverStripe\Upgrader\Composer\ComposerExec;
+
+/**
+ * Rule to update carrets constraints to the stricter tidle constraint.
+ */
+class StrictVersion implements DependencyUpgradeRule
+{
+
+    /**
+     * @inheritDoc
+     * @param  array $dependencies Dependencies to upgrade
+     * @return array Upgraded dependencies
+     */
+    public function upgrade(array $dependencies, ComposerExec $composer): array
+    {
+        $regex = '/^\^(\d+\.\d+)(\.[0-9a-z]+)?/';
+
+        foreach ($dependencies as &$constraint) {
+            // Loop through each cosntraint and try to find the one that start with a carret.
+            $constraint = preg_replace_callback($regex, function($matches) {
+
+                // Find out if we have missing digit at the end
+                $finalDigit = $matches[2] ?? '.0';
+
+                // If our missing digit is an actual digit
+                if (preg_match('/^\.\d+$/', $finalDigit)) {
+                    return '~' . $matches[1] . ($matches[2] ?? '.0');
+                } else {
+                    // Handle weird edge case where the last digit is not a digit. e.g.: ^1.2.x-dev
+                    return $matches[0];
+                }
+
+            }, $constraint);
+        }
+
+        return $dependencies;
+    }
+}
---
+++ tests/Composer/Rules/StrictVersionTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SilverStripe\Upgrader\Tests\Composer\Rules;
+
+use PHPUnit\Framework\TestCase;
+use SilverStripe\Upgrader\Composer\ComposerExec;
+use SilverStripe\Upgrader\Composer\ComposerFile;
+use SilverStripe\Upgrader\Composer\Rules\StrictVersion;
+use InvalidArgumentException;
+
+class StrictVersionTest extends TestCase
+{
+
+    public function testUpgrade()
+    {
+        $composer = new ComposerExec(__DIR__);
+        $rule = new StrictVersion();
+
+        $input = [
+            'package/aaa' => '1.2.3',
+            'package/aa' => '1.2',
+            'package/a' => '1',
+            'package/bbb' => '~1.2.3',
+            'package/bb' => '~1.2',
+            'package/b' => '~1',
+            'package/ccc' => '^1.2.3',
+            'package/cc' => '^1.2',
+            'package/c' => '^1',
+            'package/d' => 'dev-master',
+            'package/e' => '*',
+            'package/f' => '>=3',
+            'package/ggg' => '^1.2.3@dev',
+            'package/gg' => '^1.2@dev',
+            'package/hhh' => '^1.2.x-dev',
+        ];
+        $output = [
+            'package/aaa' => '1.2.3',
+            'package/aa' => '1.2',
+            'package/a' => '1',
+            'package/bbb' => '~1.2.3',
+            'package/bb' => '~1.2',
+            'package/b' => '~1',
+            'package/ccc' => '~1.2.3',
+            'package/cc' => '~1.2.0',
+            'package/c' => '^1',
+            'package/d' => 'dev-master',
+            'package/e' => '*',
+            'package/f' => '>=3',
+            'package/ggg' => '~1.2.3@dev',
+            'package/gg' => '~1.2.0@dev',
+            'package/hhh' => '^1.2.x-dev',
+        ];
+
+        $this->assertEquals(
+            $rule->upgrade($input, $composer),
+            $output
+        );
+    }
+}
