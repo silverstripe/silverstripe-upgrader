c59a810582daa812b7fc3bb3077fc1930531bea1
Maxime Rainville <maxime@silverstripe.com>
2018-05-14 17:25:18 +1200

    :sparkles::white_check_mark: Update Change applier to handle file operation and add matching unit test

--- src/CodeCollection/ChangeApplier.php
+++ src/CodeCollection/ChangeApplier.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace SilverStripe\Upgrader\CodeCollection;
+
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Generic implementation of CollectionInterface::applyChanges
@@ -22,9 +23,42 @@ trait ChangeApplier
      */
     public function applyChanges(CodeChangeSet $changes): void
     {
+        $fs = new Filesystem();
+
         foreach ($changes->allChanges() as $path => $change) {
+            $ops = $changes->opsByPath($path);
             $item = $this->itemByPath($path);
-            $item->setContents($change['new']);
+            $fullPath = $item->getFullPath();
+            switch ($ops) {
+                case 'deleted':
+                    $fs->remove($fullPath);
+                    break;
+                case 'renamed':
+                    $this->createDirForPath($fs, $item->getBasePath(), $change['path']);
+                    $fs->rename($fullPath, $item->getBasePath() . DIRECTORY_SEPARATOR . $change['path']);
+                    $item = $this->itemByPath($change['path']);
+                    break;
+            }
+
+            if ($changes->hasNewContents($path)) {
+                $item->setContents($change['new']);
+            }
+        }
+    }
+
+    /**
+     * Make sure the provided path exists
+     * @param FileSystem $fs
+     * @param string $basePath
+     * @param string $relativePath
+     * @return void
+     */
+    private function createDirForPath(FileSystem $fs, string $basePath, string $relativePath): void
+    {
+        $paths = explode(DIRECTORY_SEPARATOR, $relativePath);
+        array_pop($paths);
+        if ($paths) {
+            $fs->mkdir($basePath . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $paths));
         }
     }
 }
