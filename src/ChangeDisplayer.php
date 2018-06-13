d11980bf351cb1d8d50434dbfe0eda8e20a6cac6
Maxime Rainville <maxime@silverstripe.com>
2018-06-13 16:09:26 +1200

    :art: Splitting implementation details into private functions.

--- src/ChangeDisplayer.php
+++ src/ChangeDisplayer.php
@@ -21,29 +21,9 @@ class ChangeDisplayer
     public function displayChanges(OutputInterface $output, CodeChangeSet $changes): void
     {
         foreach ($changes->affectedFiles() as $path) {
-            // Display the file title
-            $ops = $changes->opsByPath($path);
-            $label = $ops ?: 'unchanged';
-            $display = $path . ($ops == 'renamed' ? ' -> ' . $changes->newPath($path) : '');
-            $output->writeln(sprintf("%s:\t<info>%s</info>", $label, $display));
-
-            // Display content change
-            if ($changes->hasNewContents($path)) {
-                // Show actual output if -v
-                $new = preg_split('~\R~u', $changes->newContents($path));
-                $old = preg_split('~\R~u', $changes->oldContents($path));
-                $diff = new \Diff($old, $new);
-                $render = $diff->render(new Diff_Renderer_Text_Unified());
-                $output->writeln("<comment>{$render}</comment>");
-            }
-
-            // Display warnings if any
-            if ($changes->hasWarnings($path)) {
-                $output->writeln("Warnings for $path:");
-                foreach ($changes->warningsForPath($path) as $warning) {
-                    $output->writeln(" - $warning");
-                }
-            }
+            $this->displayFileOperationTitle($changes, $path, $output);
+            $this->displayNewContentsForFile($changes, $path, $output);
+            $this->displayWarningsForFile($changes, $path, $output);
         }
     }
 
@@ -56,13 +36,62 @@ class ChangeDisplayer
     public function displayWarningsOnly(OutputInterface $output, CodeChangeSet $changes): void
     {
         foreach ($changes->affectedFiles() as $path) {
-            // Display warnings if any
-            if ($changes->hasWarnings($path)) {
-                $output->writeln("Warnings for $path:");
-                foreach ($changes->warningsForPath($path) as $warning) {
-                    $output->writeln(" - $warning");
-                }
+            $this->displayWarningsForFile($changes, $path, $output);
+        }
+    }
+
+    /**
+     * Display a diff of the changes for the provided file.
+     * @param CodeChangeSet $changes
+     * @param string $path
+     * @param OutputInterface $output
+     * @return void
+     */
+    private function displayFileOperationTitle(CodeChangeSet $changes, string $path, OutputInterface $output): void
+    {
+        // Display the file title
+        $ops = $changes->opsByPath($path);
+        $label = $ops ?: 'unchanged';
+        $display = $path . ($ops == 'renamed' ? ' -> ' . $changes->newPath($path) : '');
+        $output->writeln(sprintf("%s:\t<info>%s</info>", $label, $display));
+    }
+
+    /**
+     * Display a diff of the changes for the provided file.
+     * @param CodeChangeSet $changes
+     * @param string $path
+     * @param OutputInterface $output
+     * @return void
+     */
+    private function displayNewContentsForFile(CodeChangeSet $changes, string $path, OutputInterface $output): void
+    {
+        if ($changes->hasNewContents($path)) {
+            // Show actual output if -v
+            $new = preg_split('~\R~u', $changes->newContents($path));
+            $old = preg_split('~\R~u', $changes->oldContents($path));
+            $diff = new \Diff($old, $new);
+            $render = $diff->render(new Diff_Renderer_Text_Unified());
+            $output->writeln("<comment>{$render}</comment>");
+        }
+    }
+
+    /**
+     * Display warnings for the provided file, if any.
+     *
+     * @param CodeChangeSet $changes
+     * @param string $path
+     * @param OutputInterface $output
+     * @return void
+     */
+    private function displayWarningsForFile(CodeChangeSet $changes, string $path, OutputInterface $output): void
+    {
+        // Display warnings if any
+        if ($changes->hasWarnings($path)) {
+            $output->writeln("Warnings for $path:");
+            foreach ($changes->warningsForPath($path) as $warning) {
+                $output->writeln(" - $warning");
             }
         }
     }
+
 }
