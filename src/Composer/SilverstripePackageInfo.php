dd616b6a0c949ff7215afbb61bd7050c3e640111
Maxime Rainville <maxime@silverstripe.com>
2018-12-19 18:46:33 +1300

    Add some CWP packages.

--- src/Composer/SilverstripePackageInfo.php
+++ src/Composer/SilverstripePackageInfo.php
@@ -26,4 +26,19 @@ final class SilverstripePackageInfo
      * Name of the Recipe Core package.
      */
     const CMS = 'silverstripe/cms';
+
+    /**
+     * Name of the CWP Recipe Core package.
+     */
+    const CWP_RECIPE_CORE = 'cwp/cwp-recipe-core';
+
+    /**
+     * Name of the CWP Recipe Core package.
+     */
+    const CWP_RECIPE_CMS = 'cwp/cwp-recipe-cms';
+
+    /**
+     * Name of the CWP Core package.
+     */
+    const CWP_CORE = 'cwp/cwp-core';
 }
